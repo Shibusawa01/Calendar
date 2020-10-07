1d4323a100f5db9fa5cf66d3ec9dfca292195621
Shibusawa01 <68539805+Shibusawa01@users.noreply.github.com>
2020-10-07 16:49:17 +0900

    Update main.js

--- main.js
+++ main.js
@@ -138,11 +138,9 @@ console.clear();
   createCalendar();
 }
 
-      window.addEventListener("load",function(){
         var ele=document.getElementsByTagName("output")[0];
         setInterval(function(){
           //現在時刻のDateオブジェクトを生成
           var date2 = new Date();
           ele.innerHTML=date2;
       },100);
-      },false)
