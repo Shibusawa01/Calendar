4883705b2718894c91e9ac809a420fe3073a48e0
Shibusawa01 <68539805+Shibusawa01@users.noreply.github.com>
2020-10-07 16:51:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -138,9 +138,42 @@ console.clear();
   createCalendar();
 }
 
-        var ele=document.getElementsByTagName("output")[0];
-        setInterval(function(){
-          //現在時刻のDateオブジェクトを生成
-          var date2 = new Date();
-          ele.innerHTML=date2;
-      },100);
+var realTimeDatetimeDisplay = function() {
+    var dateObj        = new Date(),
+        dateYear       = dateObj.getFullYear(),
+        dateMonth      = dateObj.getMonth() + 1,
+        dateDay        = dateObj.getDate(),
+        dateWeek       = dateObj.getDay(),
+        timeHour       = dateObj.getHours(),
+        timeMinutes    = dateObj.getMinutes(),
+        timeSeconds    = dateObj.getSeconds(),
+        weekNames      = ['日', '月', '火', '水', '木', '金', '土'],
+        displayElement = document.getElementById('real-clockbox'),
+        str            = '';
+ 
+    // 一桁の場合は0を追加
+    if (timeHour < 10) {
+        timeHour = '0' + timeHour;
+    }
+    if (timeMinutes < 10) {
+        timeMinutes = '0' + timeMinutes;
+    }
+    if (timeSeconds < 10) {
+        timeSeconds = '0' + timeSeconds;
+    }
+ 
+    // 文字列の結合
+    str  = dateYear + '年' + dateMonth + '月' + dateDay + '日' + '（' + weekNames[dateWeek] + '） ';
+    str += timeHour + '時' + timeMinutes + '分' + timeSeconds + '秒';
+ 
+    // 出力
+    if (displayElement) {
+        displayElement.innerHTML = str;
+    }
+ 
+    // 繰り返し実行
+    setTimeout(realTimeDatetimeDisplay, 1000);
+};
+ 
+// 実行
+realTimeDatetimeDisplay();
